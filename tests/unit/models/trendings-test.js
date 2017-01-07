59a608a9e542776ee9c13df61e773c4ce9c7e671
Robby Mitchell <robjmitch@gmail.com>
2017-01-07 10:35:50 -0800

    Removed blueprint generated comments from unit tests

--- tests/unit/models/trendings-test.js
+++ tests/unit/models/trendings-test.js
@@ -1,12 +1,11 @@
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('trendings', 'Unit | Model | trendings', {
-  // Specify the other units that are required for this test.
   needs: []
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
-  // let store = this.store();
+
   assert.ok(!!model);
 });
--- tests/unit/services/category-test.js
+++ tests/unit/services/category-test.js
@@ -1,8 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:category', 'Unit | Service | category', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
 });
 
 // Replace this with your real tests.
