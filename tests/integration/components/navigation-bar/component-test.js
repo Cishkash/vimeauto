1ac798ffb8e547532736c390ee7ac494c634aa84
Robby Mitchell <robjmitch@gmail.com>
2017-01-07 00:52:56 -0800

    Adds testing tags and put in integration tests for the navigation bar component

--- tests/integration/components/navigation-bar/component-test.js
+++ tests/integration/components/navigation-bar/component-test.js
@@ -1,25 +1,41 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
-moduleForComponent('navigation-bar', 'Integration | Component | navigation bar', {
-  integration: true
+const selectedCategories = [
+  {
+    assignedColor: Ember.String.htmlSafe(`background-color: rgb(${255, 255, 255})`),
+    name: "Some Video",
+    uri: "/some/uri"
+  }
+];
+
+let categoryServiceStub = Ember.Service.extend({
+  selectedCategories: []
 });
 
-test('it renders', function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+moduleForComponent('navigation-bar', 'Integration | Component | navigation bar', {
+  integration: true,
+  beforeEach() {
+    this.register('service:category', categoryServiceStub);
+    this.inject.service('category', { as: 'categoryService' });
+  }
+});
 
+test('Data rendering tests', function(assert) {
+  // No categories
   this.render(hbs`{{navigation-bar}}`);
+  assert.ok(
+    this.$('[data-test="navbar-queue-no-category"]').text().includes('You haven\'t added any categories yet!'),
+    'Renders the proper name for the navbar-queue-no-category');
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#navigation-bar}}
-      template block text
-    {{/navigation-bar}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  // With categories
+  this.set('categoryService.selectedCategories', selectedCategories);
+  assert.ok(
+    this.$('[data-test="navbar-queue-category-name"]').text().includes('Some Video'),
+    'Renders the proper name for the navbar-queue-category-name');
+  assert.ok(
+    this.$('[data-test="navbar-queue-category-remove"]'),
+    'Renders a category remove button');
 });
