f589d9285cbf02c5d5df7b2a46fee51570518259
Robby Mitchell <robjmitch@gmail.com>
2017-01-15 15:00:59 -0800

    Fixes the video transition bug of previously visited video routes and minor style adjustments

--- app/components/navigation-bar/component.js
+++ app/components/navigation-bar/component.js
@@ -10,5 +10,30 @@ const {inject} = Ember;
  */
 export default Ember.Component.extend({
   // Service injections
-  categoryService: inject.service('category')
+  categoryService: inject.service('category'),
+
+  actions: {
+    /**
+     * Transitions to a random video in the category list of the user selected
+     * category.
+     * @TODO: Remove the possibility of getting the same video twice.
+     *
+     * @method transitionRandomVideo
+     * @returns {undefined}
+     */
+    transitionRandomVideo(videos) {
+      let r = Math.floor(Math.random() * 10);
+
+      Ember.getOwner(this).lookup('router:main').transitionTo('videos', videos[r].key);
+    },
+    /**
+     * Transitions to the video route of the provided ID.
+     *
+     * @method transitionToVideo
+     * @returns {undefined}
+     */
+    transitionToVideo(video) {
+      Ember.getOwner(this).lookup('router:main').transitionTo('videos', video);
+    }
+  }
 });
--- app/routes/videos.js
+++ app/routes/videos.js
@@ -1,7 +1,34 @@
 import Ember from 'ember';
 
+/**
+ * Videos route
+ *
+ * @class Route.Videos
+ * @constructor
+ * @extends Ember.Route
+ */
 export default Ember.Route.extend({
+  /**
+   * Fetches the video model for the provided video id.
+   *
+   * @event model
+   * @return {Promise}
+   */
   model(params) {
     return this.store.findRecord('video', params.video_id);
+  },
+  /**
+   * After model hook that unloads the record from the store after the model is
+   * initially loaded on to the controller. Needed because the store keeps
+   * retaining the model of the previous dynamic route after revisiting a
+   * route.
+   *
+   * @event afterModel
+   * @return {undefined}
+   */
+  afterModel(model) {
+    this.store.findRecord('video', model.id).then( video => {
+      this.store.unloadRecord(video);
+    });
   }
 });
